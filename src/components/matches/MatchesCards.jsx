886152f34cc2fb8e4afcc510e03449c01acd672f
Sumant-Dusane <sumant.dusane1966@gmail.com>
2025-08-04 19:42:16 +0530

    [Matches Card] padding fix

--- src/components/matches/MatchesCards.jsx
+++ src/components/matches/MatchesCards.jsx
@@ -21,77 +21,77 @@ import { Link } from 'react-router-dom';
 
 const matchData = [
     {
-      matchNo: "Match 1",
-      teamA: 'Team A',
-      logoA: logo1,
-      scoreA: 1,
-      teamB: 'Team B',
-      logoB: logo2,
-      scoreB: 2,
-      status: 'FINAL',
-      venue: "Kalinga Stadium",
-      date: "24 July 2025",
-      Time: "8:00 PM"
+        matchNo: "Match 1",
+        teamA: 'Team A',
+        logoA: logo1,
+        scoreA: 1,
+        teamB: 'Team B',
+        logoB: logo2,
+        scoreB: 2,
+        status: 'FINAL',
+        venue: "Kalinga Stadium",
+        date: "24 July 2025",
+        Time: "8:00 PM"
     },
     {
-      matchNo: "Match 2",
-      teamA: 'Thunder Kings',
-      logoA: logo3,
-      scoreA: 3,
-      teamB: 'Desert Bulls',
-      logoB: logo1,
-      scoreB: 3,
-      status: 'DRAW',
-      venue: "Kalinga Stadium",
-      date: "24 July 2025",
-      Time: "8:00 PM"
+        matchNo: "Match 2",
+        teamA: 'Thunder Kings',
+        logoA: logo3,
+        scoreA: 3,
+        teamB: 'Desert Bulls',
+        logoB: logo1,
+        scoreB: 3,
+        status: 'DRAW',
+        venue: "Kalinga Stadium",
+        date: "24 July 2025",
+        Time: "8:00 PM"
     },
     {
-      matchNo: "Match 2",
-      teamA: 'Thunder Kings',
-      logoA: logo2,
-      scoreA: 3,
-      teamB: 'Desert Bulls',
-      logoB: logo3,
-      scoreB: 3,
-      status: 'DRAW',
-      venue: "Kalinga Stadium",
-      date: "24 July 2025",
-      Time: "8:00 PM"
+        matchNo: "Match 2",
+        teamA: 'Thunder Kings',
+        logoA: logo2,
+        scoreA: 3,
+        teamB: 'Desert Bulls',
+        logoB: logo3,
+        scoreB: 3,
+        status: 'DRAW',
+        venue: "Kalinga Stadium",
+        date: "24 July 2025",
+        Time: "8:00 PM"
     },
     {
-      matchNo: "Match 100",
-      teamA: 'Noida Warriors',
-      logoA: logo1,
-      scoreA: 33,
-      teamB: 'Lucknow Lions',
-      logoB: logo2,
-      scoreB: 33,
-      status: 'FINAL',
-      venue: "Kalinga Stadium",
-      date: "24 July 2025",
-      Time: "8:00 PM"
+        matchNo: "Match 100",
+        teamA: 'Noida Warriors',
+        logoA: logo1,
+        scoreA: 33,
+        teamB: 'Lucknow Lions',
+        logoB: logo2,
+        scoreB: 33,
+        status: 'FINAL',
+        venue: "Kalinga Stadium",
+        date: "24 July 2025",
+        Time: "8:00 PM"
     },
-  ];
+];
 
 const MatchesCards = () => {
 
     const matchCardRef = useRef();
     const windowWidth = window.innerWidth;
 
-  return (
-    <div className="slide-container">
-        <div className="container">
+    return (
+        <div className="slide-container">
+            <div className="container">
 
-            <div className="heading-container ">
-                <h2 className='heading'>FIXTURES</h2>
-                <Link className='link-white' to={'/fixtures'}>
-                    <span>View More</span>
-                    <img src={arrow} alt="Arrow" />
-                </Link>
-            </div>
+                <div className="heading-container ">
+                    <h2 className='heading'>FIXTURES</h2>
+                    <Link className='link-white' to={'/fixtures'}>
+                        <span>View More</span>
+                        <img src={arrow} alt="Arrow" />
+                    </Link>
+                </div>
 
-            {/* <Swiper
+                {/* <Swiper
                 modules={[Navigation, Pagination]}
                 slidesPerView={3}
                 spaceBetween={25}
@@ -164,84 +164,84 @@ const MatchesCards = () => {
                 <div className="swiper-pagination" />
             </Swiper> */}
 
-            <Splide
-              ref={matchCardRef}
-              options={{
-                type: "slider",
-                pagination: false,
-                arrows: false,
-                perPage: 3,
-                perMove: 1,
-                snap: true,
-                breakpoints: {
-                320: { perPage: 1, trimSpace: false },
-                460: { perPage: 1, trimSpace: false },
-                560: { perPage: 1, trimSpace: false },
-                1024: { perPage: 3, },
-                1300: { perPage: 3, },
-                },
-            }}
-            >
-            {matchData.map((match, index) => (
-                <SplideSlide 
-                
+                <Splide
+                    ref={matchCardRef}
+                    options={{
+                        type: "slider",
+                        pagination: false,
+                        arrows: false,
+                        perPage: 3,
+                        perMove: 1,
+                        snap: true,
+                        breakpoints: {
+                            1300: { perPage: 3, },
+                            1024: { perPage: 3, },
+                            560: { perPage: 1, trimSpace: false },
+                            460: { perPage: 1, trimSpace: false },
+                            320: { perPage: 1, trimSpace: false },
+                        },
+                    }}
                 >
-                    <div className="match-card">
-                        <div className="match-header">
-                            <div className="match_no">
-                                <div>
-                                    <p className='fs-14 kbp'>Kabaddi Champion League, Haryana</p>
-                                    <p className='fs-12'>{match.date} | {match.Time}</p>
+                    {matchData.map((match, index) => (
+                        <SplideSlide
+                            key={index}
+                        >
+                            <div className="match-card">
+                                <div className="match-header">
+                                    <div className="match_no">
+                                        <div>
+                                            <p className='fs-14 kbp'>Kabaddi Champion League, Haryana</p>
+                                            <p className='fs-12'>{match.date} | {match.Time}</p>
+                                        </div>
+                                        <div className='button-purple'>
+                                            <p className='fs-12'>Day 18, {match.matchNo}</p>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div className='button-purple'>
-                                    <p className='fs-12'>Day 18, {match.matchNo}</p>
+
+                                <div className="match-body">
+                                    <div className="team">
+                                        <img src={match.logoA} alt={match.teamA} />
+                                        <p className='fs-12'>Panipat Panthers</p>
+                                    </div>
+
+                                    <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', gap: '5px' }}>
+                                        <p style={{ marginTop: '10px' }} className='fs-9'>Full Time</p>
+                                        <div className="score-section">
+                                            <div className="score">
+                                                <span>{match.scoreA}</span> - <span>{match.scoreB}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="team">
+                                        <img src={match.logoB} alt={match.teamB} />
+                                        <p className='fs-12'>Panipat Panthers</p>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>
 
-                        <div className="match-body">
-                            <div className="team">
-                                <img src={match.logoA} alt={match.teamA} />
-                                <p className='fs-12'>Panipat Panthers</p>
+
                             </div>
 
-                            <div style={{display: 'flex', alignItems: 'center', flexDirection: 'column', gap: '5px'}}>
-                                <p style={{marginTop: '10px'}} className='fs-9'>Full Time</p>
-                                <div className="score-section">
-                                <div className="score">
-                                    <span>{match.scoreA}</span> - <span>{match.scoreB}</span>
-                                </div>
-                                </div>
-                            </div>
+                        </SplideSlide>
 
-                            <div className="team">
-                                <img src={match.logoB} alt={match.teamB} />
-                                <p className='fs-12'>Panipat Panthers</p>
-                            </div>
-                        </div>
+                    ))}
+                    <SplideSlide></SplideSlide>
+                </Splide>
+                <div className='arrow-container arrow-container-right' onClick={() => {
+                    matchCardRef.current.go("+1");
+                }} >
+                    <img src={arrow2} />
+                </div>
 
-                        
-                        </div>
-                    
-                </SplideSlide>
-
-            ))}
-            <SplideSlide></SplideSlide>
-          </Splide>
-          <div className='arrow-container arrow-container-right'  onClick={() => {
-                matchCardRef.current.go("+1");
-            }} >
-            <img src={arrow2} />
-            </div>
-
-            <div className='arrow-container arrow-container-left' onClick={() => {
+                <div className='arrow-container arrow-container-left' onClick={() => {
                     matchCardRef.current.go("-1");
                 }} >
-                <img src={arrow2}  />
+                    <img src={arrow2} />
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 };
 
 export default MatchesCards;
